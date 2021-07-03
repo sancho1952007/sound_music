93d7e625501105d72e18768209b84a5cd61f8966
Sancho Godinho <73981314+sancho1952007@users.noreply.github.com>
2021-07-03 08:06:49 +0530

    Update __main__.py

--- sound_music/__main__.py
+++ sound_music/__main__.py
@@ -2,6 +2,7 @@ from sound_music import *
 import os
 print("Use the play() method to play music using sound_music\nFor more info visit: https://sites.google.com/view/soundmusic")
 print("\nGo to the directory where you have music files and run 'python -m sound_music' This will play the songs automatically in the directory!")
+print("\nOfficial Repo: https://github.com/sancho1952007/sound_music")
 print("\nCopyright (c) Sancho Godinho-sound_music")
 for music in os.listdir():
     if music.endswith(".mp3"):
